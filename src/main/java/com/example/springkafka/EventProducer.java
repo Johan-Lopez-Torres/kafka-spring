d618bac7ad5ab486071f6a7214d70a3654da6bf0
Your Name <you@example.com>
2024-11-17 00:01:42 -0500

    i just remember having changed the app props :(

--- src/main/java/com/example/springkafka/EventProducer.java
+++ src/main/java/com/example/springkafka/EventProducer.java
@@ -9,13 +9,13 @@ public class EventProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private static final String TOPIC = "event-topic";
 
+
     public EventProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void produceEvent(String aggregateId, String eventType, String payload) {
-        String eventMessage = String.format("{\"aggregateId\":\"%s\", \"eventType\":\"%s\", \"payload\":\"%s\"}",
-                aggregateId, eventType, payload);
+        String eventMessage = String.format("{\"aggregateId\":\"%s\", \"eventType\":\"%s\", \"payload\":\"%s\"}", aggregateId, eventType, payload);
         kafkaTemplate.send(TOPIC, aggregateId, eventMessage);
     }
 }
